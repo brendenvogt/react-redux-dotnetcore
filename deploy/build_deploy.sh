c3120171560518435053fd305808c39bf4b6648c
Brenden Vogt <bvogtdev@gmail.com>
2019-07-25 23:05:43 -0700

    added helpers

--- deploy/build_deploy.sh
+++ deploy/build_deploy.sh
@@ -1,4 +1,4 @@
 version=$1
 docker build -t brendenvogt/reactreduxdotnetcore:$version ./src
 docker push brendenvogt/reactreduxdotnetcore:$version
-helm upgrade web charts/web --set image.tag=$version
+helm upgrade --install web charts/web --set image.tag=$version
